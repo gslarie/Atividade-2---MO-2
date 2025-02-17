a79c90e72b5d8462b7fe4fe36b95f72b43f22c73
gslarie <larissa.gs201373@gmail.com>
2025-02-17 13:52:49 -0300

    First Commit

---
+++ src/teste1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class teste1 {
+
+public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Digite o nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite a idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite o telefone: ");
+        String phone = scanner.nextLine();
+
+        System.out.println("Digite o CPF: ");
+        String cpf = scanner.nextLine();
+
+        System.out.println("Digite o RG: ");
+        String rg = scanner.nextLine();
+
+        System.out.println("Digite o endereço: ");
+        String endereco = scanner.nextLine();
+
+        System.out.println("Digite o sexo (M/F): ");
+        String sexo = scanner.nextLine();
+
+}
+
+}
